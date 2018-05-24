bb142a55ce144c7a2a816ea1b298a69ec5841ea7
Pabblo <pawel.mat17@op.pl>
2018-05-24 13:38:02 +0200

    Cos tam

--- Projekcik/Projekcik/Bilet.cs
+++ Projekcik/Projekcik/Bilet.cs
@@ -8,8 +8,15 @@ namespace Projekcik
 {
     class Bilet : Rezerwacja
     {
+        // ma konstruktora po prostu
+
+
         private String NrBiletu;
 
+        /// <summary>
+        /// Setter do numeru biletu,, będzie troche działał jak Konstruktor
+        /// </summary>
+        /// <param name="numer"></param>
         public void SetNrBiletu(String numer)
         {
             NrBiletu = numer;
--- Projekcik/Projekcik/Rezerwacja.cs
+++ Projekcik/Projekcik/Rezerwacja.cs
@@ -8,6 +8,27 @@ namespace Projekcik
 {
     class Rezerwacja
     {
+        private Klient Pasazer;
+
+        private String NrRezerwacji;
+        private int NrMiesca;
+        private int CenaBiletu;
+        private Boolean BiletVIP;
+
+        public Rezerwacja(String NrRezer,int NumerMiejs,int _Cena,Boolean Vip, Klient KtoRezerw)
+        {
+            Pasazer = KtoRezerw;
+            NrRezerwacji = NrRezer;
+            NrMiesca = NumerMiejs;
+            CenaBiletu = _Cena;
+            BiletVIP = Vip;
+        }
+
+
+        public String GetNrRezerwacji()
+        {
+            return NrRezerwacji;
+        }
 
 
 
